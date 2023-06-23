5bf76f870ea384c8bf4d732cfb0d5a80c7d3e3d3
tjsdn9803 <leeinjoo2@inu.ac.kr>
2023-06-23 21:55:48 +0900

    Update, Delete 구현

--- src/main/java/com/sparta/memo/Controller/MemoController.java
+++ src/main/java/com/sparta/memo/Controller/MemoController.java
@@ -43,4 +43,29 @@ public class MemoController {
         return responseList;
     }
 
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        // 해당 id의 메모가 DB에 존재하는지 확인
+        if(memoList.containsKey(id)){
+            Memo memo = memoList.get(id);
+
+            //메모 수정
+            memo.update(requestDto);
+            return memo.getId();
+        }else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+    @DeleteMapping("memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        // 해당 id의 메모가 DB에 존재하는지 확인
+        if(memoList.containsKey(id)){
+            memoList.remove(id);
+            return id;
+        }else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
 }
